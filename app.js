fe6a0dce73f2b25376b3ba6896308045f38b41dd
Scarley <scarley809@gmail.com>
2026-01-28 18:53:22 +0100

    funcion restar

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ function sumar(num1, num2) {
 }
 
 console.log(sumar(6, 4));
+
+function restar(num1, num2) {
+    let resta = (num1 - num2);
+    let resultado = "El resultado de la resta es " + resta;
+    return resultado;
+}
+
+console.log(restar(6, 4));
